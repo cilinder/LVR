d14440de17baa56317f99883ee8f2388348f22f5
cilinder <6967728+cilinder@users.noreply.github.com>
2020-03-27 23:42:52 +0100

    update
    update

--- SATsolver_optimized.py
+++ SATsolver_optimized.py
@@ -49,20 +49,56 @@ def find_units_and_simplify(sentence, val):
 
     return sentence, val
 
-def DPLL(sentence, index, decision_stack, assignments):
+def backtrack(decision_stack, decision_level, propagation_queue):
+    decision_stack.pop()
+    propagation_queue.clear()
+    return decision_level-1
 
-    simplifiedSentence = copy.deepcopy(sentence)
-    simplifiedSentence, vals = find_units_and_simplify(simplifiedSentence, [])
+def has_unassigned_variables(assignments):
 
-    for val in vals:
-        if val > 0:
-            assignments[val-1] = True
-        else:
-            assignments[-val-1] = False
+    for i in assignments:
+        if i is None:
+            return True
+    return False
 
-    decision_stack.append(vals)
+def DPLL(sentence, nbvar, nbclauses):
+    variables = [i + 1 for i in range(nbvar)]
 
-    
+    assignments = [None] * nbvar
+    decision_stack = []
+    propagation_queue = []
+    decision_level = 0
+
+    watchlist = create_watchlist(sentence, nbvar, nbclauses)
+
+    while has_unassigned_variables(assignments):  # not all variables have been assigned a value
+        x = pick_variable(assignments)
+        propagation_queue.append(-x)  # add -x to the list of variables that cannot be watched
+        decision_stack.append([x])
+
+        assignments[abs(x)-1] = x
+
+        decision_level += 1
+        # now propagate this decision
+        while propagate(sentence, assignments, decision_stack, propagation_queue, watchlist, nbclauses) == "CONFLICT":
+            if decision_level == 0:
+                return (False, None)
+            decision_level -= 1
+            propagation_queue.clear()
+            current = decision_stack[decision_level]
+            decision_stack.pop()
+            x = current[0]
+            for i in current:
+                assignments[abs(i)-1] = None
+
+            assignments[abs(x)-1] = -x
+            if decision_level > 0:
+                decision_stack[-1].append(-x)
+            else:
+                decision_stack.append([-x])
+            propagation_queue.append(x)
+
+    return (True, assignments)
 
 def eliminate_redundant_clauses(sentence):
 
@@ -114,14 +150,53 @@ def create_index(sentence, nbvar):
                 index[-var-1][1].append(clause)
     return index
 
+# the method for deciding which variable we will try next
+def pick_variable(assignments):
+    for i, var in enumerate(assignments):
+        if var is None:
+            return i+1
+
+def create_watchlist(sentence, nbvar, nbclauses): # we assume every clause has at least 2 literals, otherwise it can be propagated already
+    watchlist = [None] * nbclauses
+    for i in range(nbclauses):
+        watchlist[i] = [sentence[i][0]]
+        watchlist[i].append(sentence[i][1])
+    return watchlist
+
+def propagate(sentence, assignments, decision_stack, prop_queue, watchlist, nbclauses):
+
+    inside_assignments = []
+    while prop_queue:
+        x = prop_queue.pop()
+        for i in range(nbclauses):
+            for j in [0,1]:
+                if x == watchlist[i][j]:
+                    y = watchlist[i][1-j]
+                    if y in assignments:
+                        break
+                    found_another = False
+                    for var in sentence[i]:
+                        if var != x and var != y and -var not in assignments:
+                            watchlist[i][j] = var
+                            found_another = True
+                    if not found_another:
+                        if -y in assignments:
+                            for k in inside_assignments:
+                                assignments[abs(k)-1] = None
+                            return "CONFLICT"
+                        else:
+                            assignments[abs(y)-1] = y
+                            inside_assignments.append(y)
+                            prop_queue.append(-y)
+
+    return "DONE"
 
 if __name__ == "__main__":
 
     time1 = time.time()
 
-    for i in range(1):
-        filename='.\\tests\\CBS_k3_n100_m403_b10_' + str(i+1) + '.cnf'
-        filename = 'test.txt'
+    for i in range(100):
+        filename='.\\tests\\uf20-0' + str(i+1) + '.cnf'
 
         file = open(filename, "r")
         lines = file.readlines()
@@ -129,27 +204,19 @@ if __name__ == "__main__":
         sentence, nbvar, nbclauses = parse_dimacs(lines)
         sentence.sort(key=len)
         sentence = eliminate_redundant_clauses(sentence)
+        sentence, val = find_units_and_simplify(sentence, [])
 
-        assignments = [None] * nbvar
-        decision_stack = []
+        nbclauses = len(sentence)
 
-        index = create_index(sentence, nbvar)
+        assignments = []
 
-        success, val = DPLL(sentence, index, decision_stack, assignments)
-        #outputfilename = sys.argv[2]
-        #outputfile = open(outputfilename, "w")
-        if success:
-            continue
-            #print('SAT')
-            #for value in val:
-                #outputfile.write("%i " % value)
-        else:
-            print('NOSAT')
-            #outputfile.write('0')
+        if len(val) < nbvar:
+            sat , assignments = DPLL(sentence, nbvar, nbclauses)
+            if not sat:
+                print("UNSAT")
+                continue
 
-        #outputfile.close()
-
-        #validate.validate(sentence, val)
+        validate.validate(sentence, assignments + val)
 
     time2 = time.time()
     print('the function took {:.3f} s'.format((time2 - time1)))
